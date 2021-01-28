3b512de40e35cd8ad0cc1e2171ecf0d4a5d7b61b
pdh1124 <pdw3231@naver.com>
2021-01-28 09:07:45 +0900

    테스트 공부

---
+++ le_js/test.js
@@ -0,0 +1,33 @@
+'use strict';
+console.clear();
+
+class Counter {
+    constructor(runEveryFiveTimes) {
+        this.counter = 0;
+        this.callback = runEveryFiveTimes;
+    }
+
+    increase() {
+        this.counter++;
+        console.log(this.counter);
+        if(this.counter % 5 === 0) {
+            this.callback && this.callback(this.counter);
+        }
+    }
+}
+
+function printSomething(num) {
+    console.log(`yo! ${num}`);
+}
+
+function alertNum(num) {
+    alert(`wow! $`);
+}
+
+const printCounter = new Counter(printSomething);
+const alertCounter = new Counter(alertNum);
+
+printCounter
+
+
+
